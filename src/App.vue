d39ae2b5cd5fbb351f75e4f876127be45f704beb
James Sral <james.sral@gmail.com>
2018-02-24 18:37:21 -0500

    Add vue apollo library for graphql; scaffold navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav-bar></nav-bar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/NavBar/NavBar.vue'
 export default {
+  components: {
+    NavBar,
+  },
   name: 'App'
 }
 </script>
 
 <style>
+body {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+}
 </style>
---
+++ src/components/common/NavBar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    MealPlanner
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+.container {
+  --horizontal-padding: 1em;
+  background-color: #22AA88;
+  display: flex;
+  align-items: center;
+  font-size: 1.2em;
+  height: 100%;
+  max-height: 2.5em;
+  padding-left: var(--horizontal-padding);
+  padding-right: var(--horizontal-padding);
+  position: fixed;
+  width: 100vw;
+}
+</style>
